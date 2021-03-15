fee02bc071213b4c467cf5d9c48df6825c7def07
DenBond7 <DenBond7@gmail.com>
2021-03-15 12:44:17 +0200

    Modified ci-setup-and-run-emulator.sh.| #1084

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo no | avdmanager -v create avd --name ci-test-pixel-x86-api30 --package "system-images;android-30;google_apis;x86" --device 'pixel_xl' --abi 'google_apis/x86'
-emulator -avd ci-test-pixel-x86-api30 -noaudio -no-boot-anim -gpu off -no-window &
+emulator -avd ci-test-pixel-x86-api30 -noaudio -no-boot-anim -no-window &
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 adb shell wm dismiss-keyguard
 sleep 1
