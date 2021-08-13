8ac9bf8cbb008194dc08fc4c7108aef17a96c9eb
DenBond7 <DenBond7@gmail.com>
2021-08-13 08:43:46 +0300

    Test commit

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -7,4 +7,4 @@ adb shell settings put global window_animation_scale 0
 adb shell settings put global transition_animation_scale 0
 adb shell settings put global animator_duration_scale 0
 
-echo "Emulator is ready"
+echo "Emulator is ready."
